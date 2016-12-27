f50fb5bc868bb6c024399ad8b86ae89dd74de8de
Boris Simeonov <BorisSimeonov@Virtual.Space>
2016-12-27 16:55:07 +0200

    Add project files.

---
+++ ReverseNumbersInStack/ReverseNumbersInStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseNumbersInStack
+{
+    public class ReverseNumbersInStack
+    {
+        public static void Main()
+        {
+            var numberStack = new Stack<string>();
+            var input = Console.ReadLine();
+
+            if (input != null)
+            {
+                input.Split(' ')
+                .ToList()
+                .ForEach(item => numberStack.Push(item));
+            }
+                
+            Console.WriteLine(string.Join(" ", numberStack.Select(num => num.ToString())));
+        }
+    }
+}
